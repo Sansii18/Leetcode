0582090ae8230915606a3e7f853bed50464abfb5
Sansii18 <173576131+Sansii18@users.noreply.github.com>
2026-04-13 18:25:36 +0530

    Time: 7 ms (74.18%) | Memory: 49.8 MB (39.53%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,16 +1,11 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> list = new ArrayList<>();
-        // if(strs.length == 0){
-        //     list.add(new ArrayList<>(ans));
-        // }
-
         HashMap <String, List<String>> map = new HashMap<>();
         for(String val : strs){
             String s = sort(val);
             map.computeIfAbsent(s, k-> new ArrayList<>()).add(val);
         }
-
         list.addAll(map.values());
         return list;
     }
