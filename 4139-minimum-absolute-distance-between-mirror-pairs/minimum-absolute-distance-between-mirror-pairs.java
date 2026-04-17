f35aa424328fbcfb60add25db24a2a592f251b67
Sansii18 <173576131+Sansii18@users.noreply.github.com>
2026-04-18 01:57:47 +0530

    Time: 52 ms (54.27%) | Memory: 95.1 MB (60.80%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
@@ -15,7 +15,7 @@ class Solution {
 
     private int reverse(int num) {
         int rev = 0;
-        while (num != 0) {
+        while (num > 0) {
             int digit = num % 10;     
             rev = rev * 10 + digit;   
             num = num / 10;       
